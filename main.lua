7a82e2eefa3411916e50fcb5f28bcd05594a3cd1
a1bara <145414188+a1bara@users.noreply.github.com>
2025-09-06 22:57:14 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,3 @@
-
-
 -- if not getgenv().directory then
 --     game.Players.LocalPlayer:Kick("Script directory was not found!\nNOTE: THE SERVER MAY BE DOWN \nplease dm 1catlol if you have entered yours correctly")
 --     wait(3)
